8702d4d1727cb0c6d52dbea47d55f6b7999ce48f
Monish <66905892+monish-mnjds@users.noreply.github.com>
2020-11-25 12:52:46 +0530

    Create webbrowser.py

---
+++ webbrowser.py
@@ -0,0 +1,17 @@
+import webbrowser
+
+
+ecourt = 'https://districts.ecourts.gov.in/'
+
+pib = 'https://www.pib.gov.in/'
+
+lawmin = 'http://lawmin.gov.in/'
+
+indiacode = 'https://www.indiacode.nic.in/'
+
+supreme_court = 'https://main.sci.gov.in/'
+
+parliament_bill = 'https://www.prsindia.org/'
+
+
+webbrowser.open(ecourt)
